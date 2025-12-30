78967a77d4cccba5b3c692832337f033ce5879e0
Mangoxxy <Mangoxxy@users.noreply.github.com>
2025-12-30 23:16:35 +0800

    Switch to Astro Fuwari

---
+++ src/config.ts
@@ -0,0 +1,58 @@
+import type {
+  ExpressiveCodeConfig,
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+  title: "GinRaku",
+  subtitle: "By",
+  lang: "zh_CN",
+  themeColor: {
+    hue: 250,
+    fixed: false,
+  },
+  banner: {
+    enable: false,
+    src: "assets/images/demo-banner.png",
+    position: "center",
+    credit: {
+      enable: false,
+      text: "",
+      url: "",
+    },
+  },
+  toc: {
+    enable: true,
+    depth: 2,
+  },
+  favicon: [],
+};
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+  ],
+};
+
+export const profileConfig: ProfileConfig = {
+  avatar: "assets/images/demo-avatar.png",
+  name: "Mango",
+  bio: "",
+  links: [],
+};
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+  theme: "github-dark",
+};
